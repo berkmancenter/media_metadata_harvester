19fada98f1141464a3639045cda1f1b0726ce103
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2011-08-03 13:55:41 -0400

    Add additional fields to hash extracted from the XML

--- npr_scrape/npr_process_scrapped.pl
+++ npr_scrape/npr_process_scrapped.pl
@@ -6,6 +6,7 @@ use Encode;
 use LWP::UserAgent;
 use XML::LibXML;
 use Carp;
+
 #use XML::LibXML::Enhanced;
 
 use DBIx::Simple;
@@ -42,14 +43,67 @@ sub _get_db
 
 sub get_hash_from_string
 {
-    my ($xml_string) = @_;
+    my ( $xml_string ) = @_;
 
-     my $dom = XML::LibXML->load_xml( string => $xml_string );
+    my $dom = XML::LibXML->load_xml( string => $xml_string );
 
-    my $story =  $dom->documentElement();
-    my $xc = XML::LibXML::XPathContext->new( $story );
+    my $story = $dom->documentElement();
+    my $xc    = XML::LibXML::XPathContext->new( $story );
 
-    my $ret = _get_element_values_as_hash( $xc, [ qw ( title subtitle shortTitle teaser miniTeaser slug storyDate pubDate lastModifiedDate keywords priorityKeywords organization/name organization/website ) ] );
+    my $ret = _get_element_values_as_hash(
+        $xc,
+        [
+            qw (
+              title
+              subtitle
+              shortTitle
+              teaser
+              miniTeaser
+              slug
+              thumbnail.medium
+              thumbnail.large
+              thumbnail.provider
+              toenail
+              toenail.medium
+              storyDate
+              pubDate
+              lastModifiedDate
+              keywords
+              priorityKeywords
+              byline.name
+              #<organization orgAbbr="NPR" orgId="1">
+              organization.name
+              organization.website
+              audio.duration
+              audio.description
+              audio.format.mp3
+              audio.format.rm
+              audio.format.wm
+              #image
+              #image.caption
+              #image.link
+              #image.producer
+              #image.provider
+              #image.copyright
+              #relatedLink
+              #relatedLink.caption
+              #relatedLink.link
+              #pullQuote
+              #pullQuote.person
+              #pullQuote.date
+              #text
+              #text.paragraph
+              #textWithHtml
+              #textWithHtml.paragraph
+              #listText
+              #listText.item
+              #correction
+              correction.correctionTitle
+              correction.correctionText
+              correction.correctionDate
+              )
+        ]
+    );
 
     my $id = $story->getAttribute( 'id' );
 
@@ -59,6 +113,40 @@ sub get_hash_from_string
 
     #say Dumper( $dom );
     #say Dumper( $xc );
+}
+
+sub _get_attribute_value_of_xpath_query_if_exists
+{
+    my ( $xc, $query, $attribute ) = @_;
+
+    my @nodes = $xc->findnodes( $query );
+
+    #say Dumper( [ @nodes ] );
+    my $node = pop @nodes;
+
+    if ( !defined( $node ) )
+    {
+        return;
+    }
+
+    return _get_attribute_value_of_xpath_query( $xc, $query, $attribute );
+}
+
+sub _get_text_value_of_xpath_query_if_exists
+{
+    my ( $xc, $query, $attribute ) = @_;
+
+    my @nodes = $xc->findnodes( $query );
+
+    #say Dumper( [ @nodes ] );
+    my $node = pop @nodes;
+
+    if ( !defined( $node ) )
+    {
+        return;
+    }
+
+    return _get_text_value_of_xpath_query( $xc, $query );
 }
 
 sub _get_text_value_of_xpath_query
@@ -76,19 +164,19 @@ sub _get_text_value_of_xpath_query
 
 sub _get_element_values_as_hash
 {
-   my ( $xc, $element_list ) = @_;
+    my ( $xc, $element_list ) = @_;
 
-   my $ret = {};
-   foreach my $element_name ( @$element_list )
-   {
-       my $query = ".//$element_name";
-       my $value = _get_text_value_of_xpath_query ( $xc, $query );
+    my $ret = {};
+    foreach my $element_name ( @$element_list )
+    {
+        my $query = ".//$element_name";
+        my $value = _get_text_value_of_xpath_query_if_exists( $xc, $query );
 
-       $element_name =~ s/\//_/g;
-       $ret->{ $element_name } = $value;
-   }
+        $element_name =~ s/\//_/g;
+        $ret->{ $element_name } = $value;
+    }
 
-   return $ret;
+    return $ret;
 }
 
 sub _get_attribute_value_of_xpath_query
@@ -115,28 +203,29 @@ sub main
 
     my $dbh = _get_db();
 
-    my $npr_stories_xml = $dbh->query(" select * from npr_items_raw ")->hashes();
+    my $npr_stories_xml = $dbh->query( " select * from npr_items_raw " )->hashes();
 
-    foreach my $npr_story_xml (@$npr_stories_xml)
+    foreach my $npr_story_xml ( @$npr_stories_xml )
     {
+
         #say Dumper( $npr_story_xml );
 
-	#exit;
-	#say STDERR Dumper([keys %{$npr_story_xml} ] );
+        #exit;
+        #say STDERR Dumper([keys %{$npr_story_xml} ] );
 
-	my $xml_string = $npr_story_xml->{ full_xml_string };
+        my $xml_string = $npr_story_xml->{ full_xml_string };
 
-	#say $xml_string;
+        #say $xml_string;
 
-	#exit;
+        #exit;
 
-	my $hash = get_hash_from_string( $xml_string );
+        my $hash = get_hash_from_string( $xml_string );
 
-	#say STDERR Dumper( $hash );
+        #say STDERR Dumper( $hash );
 
-	#map { say "$_ text ," } sort (keys %{ $hash });
+        #map { say "$_ text ," } sort (keys %{ $hash });
 
-	#exit;
+        #exit;
 
         $dbh->insert( 'npr_items_processed', $hash );
     }
