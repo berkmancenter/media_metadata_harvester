defa557ca6243d5653dc4c952b664b10f6504842
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2011-08-17 14:51:09 -0400

    Iterate through each query ID to request 200 stories 20 at a time rather than just getting the first 20.

--- npr_scrape/npr_scrape.pl
+++ npr_scrape/npr_scrape.pl
@@ -27,59 +27,13 @@ use 5.10.0;
 use strict;
 use warnings;
 use warnings::unused;
+use Readonly;
 
-my $channel_author_counts = {};
-
-sub _get_text_value_of_xpath_query
-{
-    my ( $xc, $query ) = @_;
-
-    my @nodes = $xc->findnodes( $query );
-
-    #say Dumper( [ @nodes ] );
-    my $node = pop @nodes;
-
-    return $node->textContent();
-}
-
-sub _get_attribute_value_of_xpath_query
-{
-    my ( $xc, $query, $attribute ) = @_;
-
-    my @nodes = $xc->findnodes( $query );
-
-    #say Dumper( [ @nodes ] );
-    my $node = pop @nodes;
-
-    #say STDERR '_get_attribute_value_of_xpath_query';
-    #say STDERR $attribute;
-    #say STDERR $node->toStringC14N();
-
-    return $node->getAttribute( $attribute );
-}
-
-sub _get_attribute_value_of_xpath_query_if_exists
-{
-    my ( $xc, $query, $attribute ) = @_;
-
-    my @nodes = $xc->findnodes( $query );
-
-    #say Dumper( [ @nodes ] );
-    my $node = pop @nodes;
-
-    if ( ! defined( $node ) )
-    {
-       return;
-    }
-
-    return _get_attribute_value_of_xpath_query ( $xc, $query, $attribute );
-}
-
-sub _get_data_hash_from_youtube_video_entry
+sub _get_data_hash_from_item_entry
 {
     my ( $entry ) = @_;
 
-    #say "starting: _get_data_hash_from_youtube_video_entry";
+    #say "starting: _get_data_hash_from_item_entry";
 
     #say Dumper( $entry );
 
@@ -101,7 +55,7 @@ sub _get_data_hash_from_youtube_video_entry
 
     #say Dumper( $ret );
 
-    #say "ending:_get_data_hash_from_youtube_video_entry";
+    #say "ending:_get_data_hash_from_item_entry";
 
     return $ret;
 }
@@ -150,6 +104,8 @@ sub _get_item_db_record
     return $ret;
 }
 
+Readonly my $total_stories_per_id => 200;
+
 sub npr_api_url
 {
     my ( $base_url ) = @_;
@@ -158,21 +114,25 @@ sub npr_api_url
 
     confess unless $base_url;
 
-    my $uri = URI->new( $base_url );
-
-    # Create a request
-    my $req = HTTP::Request->new( GET => $uri );
-
     # max size
 
     my $start_index = 1;
 
-    my $max_results = 50;
+    my $numResults = 20;
 
-    my $num_results;
-
+    while ( $start_index <= $total_stories_per_id )
     {
-        say "Requesting'$base_url' : start_index: $start_index max-results: $max_results";
+
+        my $req_url = "$base_url&startNum=$start_index&numResults=$numResults";
+
+        my $uri = URI->new( $req_url );
+
+	# Create a request
+	my $req = HTTP::Request->new( GET => $uri );
+
+
+
+        say STDERR "Requesting'$req_url' : start_index: $start_index max-results: $numResults";
 
         # Create a request
         my $req = HTTP::Request->new( GET => $uri );
@@ -195,18 +155,28 @@ sub npr_api_url
 
         my @entries = $xc->findnodes( '//list/story' );
 
-	say Dumper([@entries]);
+	#say Dumper([@entries]);
+
+	my $num_stories = scalar ( @entries );
+
+	say STDERR "Got $num_stories stories";
+
+	if ( $num_stories == 0 )
+	{
+	   last;
+	}
 
         foreach my $entry ( @entries )
         {
 
-            my $hash = _get_data_hash_from_youtube_video_entry( $entry );
+            my $hash = _get_data_hash_from_item_entry( $entry );
 
             #say Dumper ( $hash );
 
             _get_item_db_record( $hash );
         }
 
+	$start_index += $numResults;
     }
 
     return;
@@ -214,10 +184,10 @@ sub npr_api_url
 
 sub main
 {
-    my $key = $ARGV[ 0 ];
+    Readonly my $api_key => "MDAzNzI2MDAxMDEyNDczMjQ5OTUwODhmZA001";
 
-    npr_api_url('http://api.npr.org/query?id=1034,1033&apiKey=MDAzNzI2MDAxMDEyNDczMjQ5OTUwODhmZA001');
-    npr_api_url('http://api.npr.org/query?id=13&apiKey=MDAzNzI2MDAxMDEyNDczMjQ5OTUwODhmZA001');
+    npr_api_url("http://api.npr.org/query?id=1034&apiKey=$api_key");
+    npr_api_url("http://api.npr.org/query?id=13&apiKey=$api_key");
 }
 
 main();
