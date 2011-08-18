70188503c715ad9f97f2fe8b9530228234f234ee
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2011-08-18 17:55:04 -0400

    Add media_player_url field to youtube_videos table
    Make id fields in youtube_videos and youtube_videos_raw_xml unique

--- youtube_author_search/db_schema_setup.sql
+++ youtube_author_search/db_schema_setup.sql
@@ -1,7 +1,7 @@
 CREATE TABLE youtube_videos_raw_xml
 (
 	youtube_videos_raw_xml_id INTEGER primary key AUTOINCREMENT,
-	id  text not null,
+	id  text unique not null,
 	full_xml_string text not null
 )
 ;
@@ -10,11 +10,12 @@ CREATE TABLE youtube_videos_raw_xml
 CREATE TABLE youtube_videos
 (
 	youtube_videos_id INTEGER primary key AUTOINCREMENT,
-	id  text not null,
+	id  text unique not null,
 	title text not null,
 	description text not null,
 	full_string text not null,
 	channel text not null,
+	media_player_url not null,
 	rating_average integer ,
 	rating_min     integer ,
 	rating_max     integer ,
